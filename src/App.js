577463a5995f1ecfa785c6e4bc5fbe2f01b79f6f
Mayan Agarwal <mayanagarwal@ymail.com>
2021-06-13 20:47:28 +1000

    temp linked to Linkedin

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Upcoming portfolio for <code>Mayan Agarwal</code>.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/mayanagarwal/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn Profile.
         </a>
       </header>
     </div>
